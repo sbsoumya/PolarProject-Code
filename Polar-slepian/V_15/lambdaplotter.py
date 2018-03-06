74da68a36a81d57394a52f951c63a0c1cd04ce61
soumya <soumya.s.banerjee17@gmail.com>
2018-03-06 19:25:04 +0530

    daily-autogit

--- Polar-slepian/V_15/lambdaplotter.py
+++ Polar-slepian/V_15/lambdaplotter.py
@@ -17,7 +17,8 @@ import pandas as pd
 from scipy import stats, integrate
 import polarconstruct as pcon
 import lambdathreshold as lmb
-
+import math as ma
+import matlib as ml
 
 #plt.rc('text', usetex=True)
 #plt.rc('font', family='serif')
@@ -35,9 +36,9 @@ filename="./simresults/llrsgndict-1024-0p04-18-02-15_14-58-19.txt"
 LLRdict=lmb.load_LLRdict(filename)
 N=1024
 design_p=0.04
-runsim=1000
-channel_plist=[0.04,0.15,0.2,0.25]
-skip=0
+runsim=1
+channel_plist=[0.04]#,0.15,0.2,0.25]
+skip=99
 C=pl.CapacityBSC(N,design_p)
 G=int(C)
 F=N-G
@@ -55,14 +56,16 @@ for channel_p in channel_plist:
 		
 		LLRchannels=LLRdict[str(channel_p)][i][0]#[G:]
 		SentBitchannels=LLRdict[str(channel_p)][i][1]#[G:]
-		presentIrv=[lmb.f_Irv(llr,int(sentbit)) for llr,sentbit in zip(LLRchannels,SentBitchannels)]
+		presentIrv=[ml.logdomain_sum(0,llr*(2*int(sentbit)-1)) for llr,sentbit in zip(LLRchannels,SentBitchannels)]
+		
 		#print len(presentIrv)
-		plt.scatter(range(N),presentIrv,color=color[j-1])
-			
+		plt.scatter(range(N)[::skip+1],[llr/3000 for llr in LLRchannels][::skip+1],color=color[j-1])
+		plt.scatter(range(N)[::skip+1],[int(sb) for sb in SentBitchannels][::skip+1],color='k')
 		#plt.xticks(index,RI,rotation="vertical")
 		
 	#plt.hold(True)
 	plt.title("LLR for 0.04 , compound_channel=[0.04,0.15,0.2,0.25]")
+
 
 plt.show();
 
